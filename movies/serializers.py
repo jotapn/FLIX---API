22442774e7ef0ed7acb150578a6eaa967bff06f3
Pádua Neto <paduacn@gmail.com>
2024-09-19 22:32:26 -0300

    CRUD completo de Filmes

---
+++ movies/serializers.py
@@ -0,0 +1,8 @@
+import django.db
+from rest_framework import serializers
+from movies.models import Movie
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
